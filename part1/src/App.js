8b62b1c96d67d9d567d1547bc860b52a8da4686f
thewickest <graubuntu@gmail.com>
2023-01-17 16:33:42 +0100

    Handling arrays

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,9 +4,16 @@ const App = () => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
+  const [allClicks, setAllClicks] = useState([])
 
-  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAllClicks(allClicks.concat('L'))
+    setClicks({...clicks, left: clicks.left + 1})
+  }
+  const handleRightClick = () => {
+    setAllClicks(allClicks.concat('R'))
+    setClicks({...clicks, right: clicks.right + 1 })
+  }
 
   return (
     <div>
@@ -14,6 +21,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
