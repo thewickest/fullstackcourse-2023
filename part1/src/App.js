c28d5604945f5dad4396383ea36a51e55d8a4361
thewickest <graubuntu@gmail.com>
2023-01-17 15:12:45 +0100

    Stateful component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,16 @@
-const Hello = ({name,age}) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - age
-  }
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
-    <div>
-    <p>Greetings</p>
-    <Hello name='Maya' age={26+10}/>
-    <Hello name={name} age={age}/>
-  </div>
+    <div>{counter}</div>
   )
 }
 
-export default App;
+export default App
