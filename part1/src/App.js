6f963be8bc6b0b7774001b055104d09b533a36d7
thwickest <graubuntu@gmail.com>
2023-01-17 11:32:52 +0100

    Multiple Components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,15 @@
+const Hello = () => (
+  <div>
+    <p>Hello World</p>
+  </div>
+)
+
 
 const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a+b);
   return (
     <div>
-    <p>Hello world,  it is {now.toString()}</p>
-    <p>
-      {a} plus {b} is {a+b}
-    </p>
+    <p>Greetings</p>
+    <Hello/>
   </div>
   )
 }
