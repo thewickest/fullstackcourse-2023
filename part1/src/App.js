d13d401f168a263410c9f5f7bfecdf2c11fbc40a
thewickest <graubuntu@gmail.com>
2023-01-17 11:41:23 +0100

    Props- Component Hello use two props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,23 @@
-const Hello = (props) => (
-  <div>
-    <p>Hello World {props.name}</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
 
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
   return (
     <div>
     <p>Greetings</p>
-    <Hello name='George'/>
-    <Hello name='Daisy'/>
+    <Hello name='Maya' age={26+10}/>
+    <Hello name={name} age={age}/>
   </div>
   )
 }
