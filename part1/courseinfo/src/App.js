d49b4ec6c95b269df71d7f5e3d72e0ce1f99c5f3
thewickest <graubuntu@gmail.com>
2023-01-18 12:36:59 +0100

    Exercise 2.5 from part 2: Course info separate modules

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,50 +1,10 @@
+import Course from "./components/Course";
+
 const Header = ({name}) => {
   return (
     <>
       <h1>{name}</h1>
     </>
-  )
-}
-
-const Subheader = ({name}) => {
-  return (
-    <>
-      <h2>{name}</h2>
-    </>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({parts}) => {
-  /**Exercise 2.3 already done */
-  const total = parts.reduce((total, p) => total + p.exercises, 0) //in a moment
-  return (
-    <div>
-      {parts.map(p => <Part key= {p.id} part={p} />)}
-      <Total total={total} />
-    </div>
-  )
-}
-
-const Total = ({total}) => {
-  return (
-    <>
-      <p><b>total of exercises {total}</b></p>
-    </>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Subheader name={course.name}/>
-      <Content parts={course.parts}/>
-    </div>
   )
 }
 
