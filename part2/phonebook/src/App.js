12a5e2c072660f51fdbb7e64b5f1a6d487ae0326
thewickest <graubuntu@gmail.com>
2023-01-18 14:44:00 +0100

    Exercise 2.9 from part 2: Phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,33 @@
 import { useState } from 'react'
 
-const Persons = ({persons}) => {
+const People = ({people}) => {
   return (
     <>
-      {persons.map(n => <li key={n.name}>{n.name} {n.number}</li>)}
+      {people.map(n => <li key={n.name}>{n.name} {n.number}</li>)}
     </>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '55374-7473'}
+  const [people, setPeople] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filteredName, setFilteredName] = useState('')
+  const [filteredPeople, setFilteredPeople] = useState(people)
 
   const addPerson = (event) => {
     event.preventDefault()
-    const filteredPersons = persons.filter(p => p.name === newName)
-    if(filteredPersons.length > 0) alert(`${newName} already added to the notebook`)
-    else {setPersons(persons.concat({name: newName, number: newNumber}))}
+    const fp = people.filter(p => p.name === newName)
+    if(fp.length > 0) alert(`${newName} already added to the notebook`)
+    else {
+      setPeople(people.concat({name: newName, number: newNumber}))
+      setFilteredPeople(people.concat({name: newName, number: newNumber}))
+    }
   }
 
   const handleName = (event) => {
@@ -30,9 +38,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filterName = (event) => {
+    const fp = people.filter(p => p.name.toLowerCase().includes(event.target.value.toLowerCase())) //
+    setFilteredName(event.target.value)
+    setFilteredPeople(fp)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter show with <input value={filteredName} onChange={filterName}/>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleName}/></div>
         <div>number: <input value={newNumber} onChange={handleNumber}/></div>
@@ -41,7 +57,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <People people={filteredPeople} />
     </div>
   )
 }
