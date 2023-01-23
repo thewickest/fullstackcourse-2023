0ee476c4bfe65a732288210cb30da27fdaf29487
thewickest <graubuntu@gmail.com>
2023-01-23 16:43:41 +0100

    Serving static files from the backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 let notes = [
@@ -69,7 +70,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
