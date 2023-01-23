b87b2df4f2fe97b317dc3beac62efac33bd7840d
thewickest <graubuntu@gmail.com>
2023-01-23 11:17:54 +0100

    Fetching a single resource

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -26,6 +26,16 @@ app.get('/api/notes',(request,response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id',(request,response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(n => n.id === id)
+  if(note) {
+    response.json(note)
+  }else{
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
