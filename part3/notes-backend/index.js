a96d85d012d2fcc9654ef5caa1ad5e18756e8a7f
thewickest <graubuntu@gmail.com>
2023-01-25 09:38:18 +0100

    Using database into route handlers

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -2,36 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Note = require('./models/note')
+const note = require('./models/note')
 const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId +1 
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -39,37 +15,32 @@ app.post('/api/notes', (request, response) => {
   if(!body.content) {
     response.status(400).json({error:'content missing'})
   }
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date()
-  }
-  notes = notes.concat(note)
-  response.json(note)
+    important: body.important || false
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes',(request,response) => {
-  //response.json(notes)
   Note.find({}).then(result => {
     response.json(result)
   })
 })
 
 app.get('/api/notes/:id',(request,response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(n => n.id === id)
-  if(note) {
+  Note.findById(request.params.id).then(note=> {
     response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(n => n.id !== id)
-  response.status(204).end()
+  Note.deleteOne({id: request.params.id}).then(deletedNote => {
+    response.json(deletedNote)
+  })
 })
 
 const PORT = process.env.PORT || 3001
