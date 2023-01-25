4826b30a5a5eb8cf5e2a789cdb482fffef50da13
thewickest <graubuntu@gmail.com>
2023-01-25 09:57:16 +0100

    Exercise 3.13 from part 3: Phonebook database step 1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./model/person')
 const app = express()
 
 morgan.token('data',(req,res) => JSON.stringify(req.body))
@@ -64,7 +66,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(people)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
