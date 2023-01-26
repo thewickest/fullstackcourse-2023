35dbba8982f0d2ad8ee8466629d35fd69b0015c2
thewickest <graubuntu@gmail.com>
2023-01-26 11:54:35 +0100

    Moving error handler into middleware

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -31,7 +31,7 @@ app.get('/api/notes',(request,response) => {
   })
 })
 
-app.get('/api/notes/:id',(request,response) => {
+app.get('/api/notes/:id',(request,response, next) => {
   Note.findById(request.params.id)
     .then(note=> {
       if(note) {
@@ -40,10 +40,7 @@ app.get('/api/notes/:id',(request,response) => {
         response.status(404).end( )
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({error: 'malformatted id'})
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -56,3 +53,15 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
