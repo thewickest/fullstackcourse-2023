33c3f7950d4f2d7e665a2341ad7e2f88edf38804
thewickest <graubuntu@gmail.com>
2023-02-09 10:48:24 +0100

    Exercise 4.4 from part 4: Helper functions step 2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const total = (sum, actual) => {
+    return sum + actual.likes
+  }
+
+  return blogs.reduce(total, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
